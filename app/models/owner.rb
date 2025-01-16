f5936b5be51c4bc8f8cf9a48bdfed49f701188e2
Mustafa Farah <mustafafarah8@hotmail.com>
2025-01-16 14:52:11 +0000

    rails new

---
+++ app/models/owner.rb
@@ -0,0 +1,5 @@
+class Owner < ApplicationRecord
+  has_many :cars
+
+  validates :nickname, presence: true, uniqueness: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+
+
+bruce = Owner.create!(nickname: "Batman")
+Car.create!(
+  brand: "Chevrolet",
+  model: "Impala",
+  year: "1965",
+  fuel: "Petrol",
+  owner: bruce
+)
+
+peter = Owner.create!(nickname: "Spiderman")
+Car.create!(
+  brand: "Ferrari",
+  model: "Testarossa",
+  year: "1995",
+  fuel: "Petrol",
+  owner: peter
+)
+
+clark = Owner.create!(nickname: "Superman")
+Car.create!(
+  brand: "Mercedes",
+  model: "300SL Roadster",
+  year: "1987",
+  fuel: "Diesel",
+  owner: clark
+)
