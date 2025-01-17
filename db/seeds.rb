a98744c74bd8697e624583cc35bcbaec580f4589
Mustafa Farah <mustafafarah8@hotmail.com>
2025-01-17 22:34:09 +0000

    awesome garage front-end step 3 & 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ Car.create!(
   model: "Impala",
   year: "1965",
   fuel: "Petrol",
+  image_url: "https://www.kimballstock.com/pix/car/p/02/aut-21-rk3719-01p.jpg",
   owner: bruce
 )
 
@@ -18,6 +19,7 @@ Car.create!(
   model: "Testarossa",
   year: "1995",
   fuel: "Petrol",
+  image_url: "https://images.squarespace-cdn.com/content/v1/5caed8960cf57d49530e8c60/1610516381393-SQ1767N6TTUCV4FIW9FJ/art-mg-ferraritestarossa2.jpg?format=1500w",
   owner: peter
 )
 
@@ -27,6 +29,7 @@ Car.create!(
   model: "300SL Roadster",
   year: "1987",
   fuel: "Diesel",
+  image_url: "https://images.squarespace-cdn.com/content/v1/567812df7086d7c6a3ddcd61/1499355116893-2PHEH3O7T5KCOXBS4HS6/MO17_r132_032.jpg?format=1500w",
   owner: clark
 )
 
